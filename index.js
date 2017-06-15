238836be75f54b5ac1711fe335604e2f3ac1d9ca
CRE8TIVES <mazen@cre8tiv.es>
2017-06-15 16:27:46 +0300

    Create index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello NodeJS The Creative Network!')
+  response.send('Hello NodeJS The Creative Network - this is a test dont try this at home!')
 })
 
 app.listen(app.get('port'), function() {
